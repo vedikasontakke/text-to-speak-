5e7f82282ba257ff33b0fc1ac398376b36767bbe
Lenovo <vedikasontakke2003@gmail.com>
2021-02-24 16:50:51 -0300

    intial commit

---
+++ app/src/main/java/com/example/texttospeak/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.texttospeak
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener
+{
+    private var tts: TextToSpeech? = null
+    private var buttonSpeak: Button? = null
+    private var editText: EditText? = null
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSpeak = this.button_speak
+        editText = this.edittext_input
+
+        buttonSpeak!!.isEnabled = false;
+        tts = TextToSpeech(this, this)
+
+        buttonSpeak!!.setOnClickListener { speakOut() }
+    }
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            } else {
+                buttonSpeak!!.isEnabled = true
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun speakOut() {
+        val text = editText!!.text.toString()
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
+    }
+
+    public override fun onDestroy() {
+        // Shutdown TTS
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+
+}
